3d411819fb9133fd449ca423c43c6ef832394e56
Souvik Sardar <souviksardar16@gmail.com>
2022-04-16 00:03:16 +0530

    bug fix

--- Assets/Scripts/FOVObsCheckScript.cs
+++ Assets/Scripts/FOVObsCheckScript.cs
@@ -31,7 +31,7 @@ public class FOVObsCheckScript : MonoBehaviour
 
         //Collider2D[] collArray = Physics2D.OverlapCircleAll(transform.position, radius, targetMask);
         Collider2D[] collArray = new Collider2D[3];
-        Physics2D.OverlapCircleNonAlloc(transform.position, radius, collArray, targetMask);
+        int res = Physics2D.OverlapCircleNonAlloc(transform.position, radius, collArray, targetMask);
 
         /*
         for (int i = 0; i < obstaclesInRange.Length && i < collArray.Length; i++)
@@ -52,21 +52,23 @@ public class FOVObsCheckScript : MonoBehaviour
             }
         }
         */
-
-        int j = 0;
-        for (int i = 0; i < collArray.Length && j < 3; i++)
+        if (res > 0)//If resultant array is non zero.
         {
-            if (collArray[i] != null)
+            int j = 0;
+            for (int i = 0; i < collArray.Length && j < 3; i++)
             {
-                var directionToTarget = (collArray[i].transform.position - transform.position).normalized;
-                if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
+                if (collArray[i] != null)//Check if element is not null.
                 {
-                    obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
-                    j++;
-                    isObstaclesInRange = true;
+                    var directionToTarget = (collArray[i].transform.position - transform.position).normalized;
+                    if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
+                    {
+                        obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
+                        j++;
+                        isObstaclesInRange = true;
+                    }
                 }
-            }
 
+            }
         }
     }
 
