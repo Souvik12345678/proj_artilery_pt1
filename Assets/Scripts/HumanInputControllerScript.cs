bb7ab3f4d520c0939c9f9a59192b7b66c2b73e0d
Souvik Sardar <souviksardar16@gmail.com>
2022-04-18 21:56:45 +0530

    misc.

---
+++ Assets/Scripts/HumanInputControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanInputControllerScript : MonoBehaviour
+{
+    public NewTankScript tankScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            tankScript.Move(1, 0);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            tankScript.Move(-1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            tankScript.Move(0, -1);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            tankScript.Move(0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            tankScript.MuzzleRotate(-1);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tankScript.MuzzleRotate(1);
+        }
+
+    }
+}
---
+++ Assets/Scripts/NewTankScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class NewTankScript : MonoBehaviour
+{
+    public float driveSpeed;
+    public float rotateSpeed;
+    public float muzzRotateSpeed;
+    public bool isDestroyed;
+
+    Transform tankBodyTransform;
+    Transform muzzleTransform;
+
+    private void Awake()
+    {
+        tankBodyTransform = transform.GetChild(0);
+        muzzleTransform = transform.Find("tank_body/muzzle");
+    }
+
+    /// <summary>
+    /// forward 1 -> move forward, forward -1 -> movebackward,turn-> Rotate clockwise if -1 and counterclockwise if 1
+    /// </summary>
+    public void Move(int forward, int turn)
+    {
+        if (!isDestroyed)
+        {
+            transform.Translate(forward * driveSpeed * Time.deltaTime * transform.up, Space.World);
+            transform.Rotate(0, 0, turn * Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    /// <summary>
+    /// turn == 1 clockwise, turn == -1 anticlockwise
+    /// </summary>
+    public void MuzzleRotate(int turn)
+    {
+        if (!isDestroyed)
+        {
+            float angle = Vector2.Angle(tankBodyTransform.up, muzzleTransform.up);
+            float newAngle = angle + (-turn * Time.deltaTime * muzzRotateSpeed);
+
+            //muzzleTransform.Rotate(0, 0, -turn * Time.deltaTime * muzzRotateSpeed, Space.Self);
+
+            if (newAngle < 90)
+            {
+                muzzleTransform.Rotate(0, 0, -turn * Time.deltaTime * muzzRotateSpeed, Space.Self);
+            }
+
+            //Debug.Log(muzzleTransform.localRotation.eulerAngles);
+        }
+    }
+
+}
