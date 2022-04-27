eee2817d31509d61637abcefff73143bb97ab953
Souvik Sardar <souviksardar16@gmail.com>
2022-04-27 21:56:50 +0530

    misc.

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(Spawn), 1, 0.25f);
+    }
+
+    void Spawn()
+    {
+        GameObject gm = Instantiate(prefab);
+        gm.transform.position = new Vector2(Random.Range(-0.001f, 0.001f), Random.Range(-0.001f, 0.001f));
+       
+    
+    }
+   
+}
