b26f1535c45bd008357ced303475c80e945e90ed
Souvik Sardar <souviksardar16@gmail.com>
2022-04-30 16:41:21 +0530

    new obstacle detection

--- Assets/Scripts/FOVObsCheckScript.cs
+++ Assets/Scripts/FOVObsCheckScript.cs
@@ -19,8 +19,8 @@ public class FOVObsCheckScript : MonoBehaviour
 
     private void Awake()
     {
-        obstaclesInRange = new GameObject[3];
-        collArray = new Collider2D[7];
+        obstaclesInRange = new GameObject[4];
+        collArray = new Collider2D[8];
     }
 
     private void Start()
@@ -71,40 +71,84 @@ public class FOVObsCheckScript : MonoBehaviour
             obstaclesInRange[i] = collArray[i].gameObject;
         }*/
 
-        /*
-        int j = 0;
-        foreach (var item in collArray)
-        {
-            var directionToTarget = (item.transform.position - transform.position).normalized;
-            if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
-            { 
-                obstaclesInRange[j] = item.gameObject;//Add them to obstaclesinrange Array
-                j++;
-                isObstaclesInRange = true;
-            }
+    /*
+    int j = 0;
+    foreach (var item in collArray)
+    {
+        var directionToTarget = (item.transform.position - transform.position).normalized;
+        if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
+        { 
+            obstaclesInRange[j] = item.gameObject;//Add them to obstaclesinrange Array
+            j++;
+            isObstaclesInRange = true;
         }
-        */
-        /*
+    }
+    */
+    /*
+    if (res > 0)//If resultant array is non zero.
+    {
+        int j = 0;
+        for (int i = 0; i < collArray.Length && j < 3; i++)
+        {
+            if (collArray[i] != null)//Check if element is not null.
+            {
+                var directionToTarget = (collArray[i].transform.position - transform.position).normalized;
+                if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
+                {
+                    obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
+                    j++;
+                    isObstaclesInRange = true;
+                }
+            }
+
+        }
+    }
+}
+*/
+
+    /*
+    void CheckObstacle()
+    {
+        Array.Clear(obstaclesInRange, 0, obstaclesInRange.Length);
+        Array.Clear(collArray, 0, collArray.Length);
+
+        isObstaclesInRange = false;
+
+        int res = Physics2D.OverlapCircleNonAlloc(transform.position, radius, collArray, targetMask);
+
         if (res > 0)//If resultant array is non zero.
         {
             int j = 0;
-            for (int i = 0; i < collArray.Length && j < 3; i++)
+            for (int i = 0; i < collArray.Length && j < 4; i++)
             {
                 if (collArray[i] != null)//Check if element is not null.
                 {
                     var directionToTarget = (collArray[i].transform.position - transform.position).normalized;
                     if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
                     {
-                        obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
-                        j++;
-                        isObstaclesInRange = true;
+                        float distToTarget = Vector2.Distance(transform.position, collArray[i].transform.position);
+
+                        var hit = Physics2D.Raycast(transform.position, directionToTarget, distToTarget, targetMask);
+
+                        if (hit.collider != null)
+                        {
+                            if (hit.collider == collArray[i])
+                            {
+                                obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
+                                j++;
+                                isObstaclesInRange = true;
+                            }
+
+
+                        }    
                     }
                 }
 
             }
         }
+
     }
-*/
+    */
 
     void CheckObstacle()
     {
@@ -118,22 +162,42 @@ public class FOVObsCheckScript : MonoBehaviour
         if (res > 0)//If resultant array is non zero.
         {
             int j = 0;
-            for (int i = 0; i < collArray.Length && j < 7; i++)
+            for (int i = 0; i < collArray.Length && j < 4; i++)
             {
                 if (collArray[i] != null)//Check if element is not null.
                 {
                     var directionToTarget = (collArray[i].transform.position - transform.position).normalized;
                     if (Vector2.Angle(transform.up, directionToTarget) < angle)//If target in fov
                     {
-                        obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
-                        j++;
-                        isObstaclesInRange = true;
+                        if (!IsObstructed(collArray[i], directionToTarget))
+                        {
+                            obstaclesInRange[j] = collArray[i].gameObject;//Add them to obstaclesinrange Array
+                            j++;
+                            isObstaclesInRange = true;
+                        }
                     }
                 }
 
             }
         }
 
+    }
+
+    bool IsObstructed(Collider2D collider,Vector2 directionToTarget)
+    {
+        float distToTarget = Vector2.Distance(transform.position, collider.transform.position);
+
+        var hit = Physics2D.Raycast(transform.position, directionToTarget, distToTarget, targetMask);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider == collider)
+            {
+                return false;
+            }
+            return true;
+        }
+        return true;
     }
 
 }
